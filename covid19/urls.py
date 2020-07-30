df981c0372693a7b2fd2edb57ccb9f5c7ba26f61
Neeraj joon <neerajjoon007@gmail.com>
2020-07-30 15:23:31 +0530

    diploy

--- covid19/urls.py
+++ covid19/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tracker.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
