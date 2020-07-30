7fd9a75b92c27a5e44f5be227f51bf74d56586ff
Neeraj joon <neerajjoon007@gmail.com>
2020-07-30 15:45:32 +0530

    diploy

--- covid19/settings.py
+++ covid19/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,5 @@ STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
